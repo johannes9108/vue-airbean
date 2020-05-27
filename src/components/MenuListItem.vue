ee7ccb081960e74c26d20f77d68a1bad7b1f4414
Erik Lindh <erik@tymelapse.com>
2020-05-27 10:31:56 +0200

    menuItem and view implemented not styled

---
+++ src/components/MenuListItem.vue
@@ -0,0 +1,57 @@
+<template>
+<div>
+    <div class="menuItem">
+        <button v-on:click= "addItemToCart">+</button>
+        <div>
+            <h2>{{name}}</h2>
+            <p>{{description}}</p>
+        </div>
+        <h4>{{price}} kr</h4>
+    </div>
+</div>
+</template>
+<script>
+export default {
+    props:{
+    name: String,
+    description: String,
+    price: Number,
+    id: Number
+    },
+
+    data: () => ({
+
+    }),
+    
+    methods: {
+        addItemToCart(){
+             this.$store.commit('saveItemToShoppingCart', this.id);
+        }
+    },
+
+    computed:{
+
+    }
+}
+</script>
+
+<style>
+
+    .menuItem{
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+
+    
+    
+    }
+
+    .menuItem button{
+        width: 2rem;
+        height: 2rem;
+    }
+
+    h1{
+        color: black;
+    }
+</style>
--- src/views/MenuView.vue
+++ src/views/MenuView.vue
@@ -1,7 +1,19 @@
 <template>
   <div class="menuView">
+    
     <Header />
-    <section class="erikFixingThis">menu</section>
+      <section>
+        <h1>MENU</h1>
+        <MenuListItem
+          class= "menuListItem"
+          v-for= "item in getCoffeeList"
+          v-bind:key= 'item.id'
+          v-bind:name= 'item.title'
+          v-bind:description= "item.desc"
+          v-bind:price= "item.price"
+          v-bind:id= "item.id">
+        </MenuListItem>
+      </section>
     <Footer />
     <router-view />
   </div>
@@ -9,13 +21,23 @@
 <script>
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import MenuListItem from '../components/MenuListItem'
 export default {
+
   data: () => ({}),
   components: {
     Header,
-    Footer
+    Footer,
+    MenuListItem
   },
-  methods: {}
+  methods: {
+  },
+
+  computed:{
+    getCoffeeList(){
+      return this.$store.state.coffeeList;
+    }
+  }
 };
 </script>
 
@@ -26,4 +48,13 @@ export default {
   display: grid;
   grid-template-rows: 14vh minmax(77vh, auto) 9vh;
 }
+
+h1{
+  margin-bottom: 2rem;
+}
+
+.menuListItem{
+  margin-bottom: 2.2rem;
+}
+
 </style>
