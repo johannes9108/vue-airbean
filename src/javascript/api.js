d2df6a04205958afcdb92bc128c410ffc0e98a75
Johannes Hedman <johannes91.hedman@gmail.com>
2020-05-28 15:43:28 +0200

    api update

--- src/javascript/api.js
+++ src/javascript/api.js
@@ -1,46 +1,46 @@
 export default class CoffeDBAPI {
   constructor() {}
 
-  getAllCoffees() {
-    try {
-      //   let promise = fetch(this.url, {
-      //       method: "GET",
-      //   });
+  //   getAllCoffees() {
+  //     try {
+  //       //   let promise = fetch(this.url, {
+  //       //       method: "GET",
+  //       //   });
 
-      //   // console.log("Promise: " + promise);
+  //       //   // console.log("Promise: " + promise);
 
-      //   let response = await promise;
-      //   // console.log("Response: " + response);
+  //       //   let response = await promise;
+  //       //   // console.log("Response: " + response);
 
-      //   let data = await response.json();
+  //       //   let data = await response.json();
 
-      //   // console.log("Data: " + data);
-      let x = [
-        {
-          id: "1",
-          title: "Caffe Latte",
-          description: "Stirred Coffe with Milk",
-          price: "40",
-        },
-        {
-          id: "2",
-          title: "Caffe Machiatto",
-          description: "Stirred Coffe with Milk",
-          price: "60",
-        },
-        {
-          id: "3",
-          title: "Caffe Latte",
-          description: "Stirred Coffe with Milk",
-          price: "50",
-        },
-      ];
-      return x;
-    } catch (error) {
-      console.log("Corrupted Data");
-    }
-  }
-  async getOrdersForCustomer(id) {}
-  async createNewCustomer(name, email) {}
-  async checkValidCustomer() {}
+  //       //   // console.log("Data: " + data);
+  //       let x = [
+  //         {
+  //           id: "1",
+  //           title: "Caffe Latte",
+  //           description: "Stirred Coffe with Milk",
+  //           price: "40",
+  //         },
+  //         {
+  //           id: "2",
+  //           title: "Caffe Machiatto",
+  //           description: "Stirred Coffe with Milk",
+  //           price: "60",
+  //         },
+  //         {
+  //           id: "3",
+  //           title: "Caffe Latte",
+  //           description: "Stirred Coffe with Milk",
+  //           price: "50",
+  //         },
+  //       ];
+  //       return x;
+  //     } catch (error) {
+  //       console.log("Corrupted Data");
+  //     }
+  //   }
+  //   async getOrdersForCustomer(id) {}
+  //   async createNewCustomer(name, email) {}
+  //   async checkValidCustomer() {}
 }
